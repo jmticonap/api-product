7adb489fb31b8b4e5cf14f4997b4a8a3153ff4a7
juancho <jm.ticona.p@gmail.com>
2023-02-08 04:21:50 -0500

    ADD apicache

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,14 +1,17 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import express from 'express'
 import productController from '../controllers/product.controller'
+import apicache from 'apicache'
 
 const route = express.Router()
+
+ const cache = apicache.middleware
 
 // Page with list of products
 route.get('/', productController.index)
 
 // Show especific product selected by id
-route.get('/:id', productController.findById)
+route.get('/:id', cache('5 minutes'), productController.findById)
 
 // Create new product
 route.post('/', productController.create)
